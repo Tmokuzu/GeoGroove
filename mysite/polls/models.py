8636e693d0aa440d8cfb4b6517e8ddcd7959cb22
Tmokuzu <126632251+Tmokuzu@users.noreply.github.com>
2023-09-17 14:08:29 +0900

    update api

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Music(models.Model):
+    MusicTitle = models.CharField(max_length=255)
+    ArtistName = models.CharField(max_length=255)
+    Country = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.MusicTitle
+    
+    #def update_DB(self, MusicDB):
+        
